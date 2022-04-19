68c0404416f42fb7a0021c1d4772ecf4fa588496
zubastu <79757684+zubastu@users.noreply.github.com>
2022-04-19 15:37:18 +0300

    Создал предварительную структуру

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../index.css'
+import Footer from './Footer'
+import Header from './Header'
+import Main from './Main'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <div className="footer center">
+            <p className="footer__copyright">&copy; 2022 Махин Илья</p>
+        </div>
+    )
+}
+
+export default Footer

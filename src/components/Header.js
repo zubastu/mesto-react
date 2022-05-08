18bc68821be9a9f9eeecb1132ed441b20e24c4e7
zubastu <79757684+zubastu@users.noreply.github.com>
2022-05-08 22:37:21 +0300

    Реализовал отдельный компонент попап, закрытие по клику на оверлей  по нажатию на эскейп

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import headerLogo from "../images/logo__header.svg";
+import shine from "../images/shine.png";
+import moon from "../images/moon.png";
 
 function Header() {
   return (
     <div className="header center">
       <img className="header__logo" src={headerLogo} alt="Логотип" />
+      <img className="theme-image" src={moon} alt="Изменить стиль" />
     </div>
   );
 }
